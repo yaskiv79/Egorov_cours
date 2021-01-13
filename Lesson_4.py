7b76281213d005a403760b315e4788940d159aa1
Makohonchuk Valentina <yaskiv79@gmail.com>
2021-01-13 21:40:45 +0200

    Переменные, оператор присваивания

---
+++ Lesson_4.py
@@ -0,0 +1,24 @@
+a = 4
+print('переменная a =', a)
+print('a * 5 =', a * 5)
+b = 10
+print('переменная b =', b)
+print('a * b =', a * b)
+print('тип переменной а -', type(a))
+c = 3
+print(c)
+c = c * 2
+print('c = c * 2')
+print(c)
+a=b=c=f=2
+print(a, b, c, f)
+a,d,g = 1,2,3
+print(a, d, g)
+a = 7
+a = a + a
+a = a + a + a
+print(a)
+b = 3
+b = b * b
+b = b + b * b
+print(b)
