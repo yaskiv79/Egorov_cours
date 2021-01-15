8228c41aa031b2480a11d1d1612aea17c449f7bc
Makohonchuk Valentina <yaskiv79@gmail.com>
2021-01-15 11:16:25 +0200

    Деление на цело и с остатком, разложение числа на цифры

---
+++ Lesson_7.py
@@ -0,0 +1,13 @@
+x = int(input('Введите пятичзначное число: '))
+a = x // 10000
+print(a)
+b = x // 1000 % 10
+print(b)
+c = x // 100 % 10
+print(c)
+d = x // 10 % 10
+print(d)
+e = x % 10
+print(e)
+
+
